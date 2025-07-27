28bfbf840caaae56b234c9d5ae8b60a9ec6ce471
Richard Alves <richardalves232323@gmail.com>
2025-07-27 15:42:38 -0300

    :sparkles: feat:  Add new method to repository interface for updating URL click count

--- src/domain/interfaces/repositories/IUrlRespository.ts
+++ src/domain/interfaces/repositories/IUrlRespository.ts
@@ -3,7 +3,8 @@ import { Url } from "../../../domain/entities/Url";
 export interface IUrlRepository {
     save(url: Url): Promise<Url>;
     findAll(): Promise<Url[]>;
-    findBySlug(slug: string): Promise<Url>;
-    update(url: Url): Promise<Url>;
-    deleteBySlug(slug: string): Promise<Url>;
+    findBySlug(slug: string): Promise<Url | null>;
+    update(url: Url): Promise<void>;
+    updateClicksBySlug(slug: string): Promise<void>;
+    deleteBySlug(slug: string): Promise<void>;
 }
